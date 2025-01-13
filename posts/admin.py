c487c061250ef34907a53a0e05d48fd039f87d97
braginsv2 <serega2012500@yandex.ru>
2025-01-13 15:58:45 +0700

    Add files via upload

---
+++ posts/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+
+from .models import Comment, Follow, Group, Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("pk", "text", "pub_date", "author")
+    search_fields = ("text",)
+    list_filter = ("pub_date",)
+    empty_value_display = "-пусто-"
+
+
+admin.site.register(Post, PostAdmin)
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "slug", "description")
+    search_fields = ("title",)
+    empty_value_display = "-пусто-"
+
+
+admin.site.register(Group, GroupAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("pk", "author", "text")
+    search_fields = ("author",)
+    list_filter = ("created",)
+    empty_value_display = "-пусто-"
+
+
+admin.site.register(Comment, CommentAdmin)
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ("pk", "user", "author")
+    search_fields = ("author",)
+    empty_value_display = "-пусто-"
+
+
+admin.site.register(Follow, FollowAdmin)
